b98f8a1129aee2cd11735c18c1055f91e39a1a7a
Shahin Yousefi <shayousefi@users.noreply.github.com>
2016-04-15 13:09:49 -0400

    Added standard scaling, linear SVM, and PCA.

--- project.py
+++ project.py
@@ -1,31 +1,36 @@
-from sklearn.linear_model import LogisticRegressionCV, LinearRegression
+from sklearn.decomposition import RandomizedPCA
+from sklearn.linear_model import LinearRegression, LogisticRegressionCV
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 import scipy.io as sio
 import numpy as np
 
 
-def get_input_old(imgs, labels):
-    s = (1, 1024)   #each image will be a 1024 length vector
-    I = np.zeros(s)
-    L = np.zeros((1,1))
-
-    for i in range(imgs.shape[2]):
-        im = imgs[:,:,i].reshape(1, 1024)
-        I = np.vstack((I, im))    			#add the image to the matrix as the last row of the matrix
-        L = np.vstack((L, labels[i][0]))    #add the image to the matrix as the last row of the matrix
-
-    I = np.delete(I, (0), axis=0)
-    I = np.delete(L, (0), axis=0)
-
-    return I, L
-
-
-def get_input(imgs, labels):
+def get_input(imgs, labels, scaler=None):
     I = np.rollaxis(imgs, 2)
     I = np.reshape(I, (I.shape[0], -1))
+
+    I = np.asarray(I, dtype=np.float64)
+    if not scaler:
+        scaler = StandardScaler(copy=False)
+    I = scaler.fit_transform(I)
+
     L = np.ravel(labels)
-    return I, L
+    return I, L, scaler
+
+
+def get_input_pca(imgs, labels, pca=None):
+    I = np.rollaxis(imgs, 2)
+    I = np.reshape(I, (I.shape[0], -1))
+
+    if not pca:
+        pca = RandomizedPCA(n_components=None, copy=False, iterated_power=3, whiten=False)
+    I = pca.fit_transform(I)
+
+    L = np.ravel(labels)
+    return I, L, pca
 
 
 def learn_knn(training_input, training_labels, nn):
@@ -35,7 +40,7 @@ def learn_knn(training_input, training_labels, nn):
 
 
 def learn_nn(training_input, training_labels):
-    # TODO: narrow down to necessary params
+    # TODO: replace with sknn
     nn = MLPClassifier(hidden_layer_sizes=(100, ), activation='logistic', algorithm='adam', alpha=0.0001,
                        batch_size='auto', learning_rate='constant', learning_rate_init=0.001, power_t=0.5, max_iter=200,
                        shuffle=True, random_state=None, tol=0.0001, verbose=False, warm_start=False, momentum=0.9,
@@ -51,16 +56,29 @@ def learn_log_reg(training_input, training_labels):
     lr.fit(training_input, training_labels)
     return lr
 
+
 def learn_lin_reg(training_input, training_labels):
-    lr = LinearRegression(fit_intercept=False, normalize=False, copy_X=True, n_jobs=-1)
+    lr = LinearRegression(fit_intercept=True, normalize=False, copy_X=True, n_jobs=-1)
     lr.fit(training_input, training_labels)
     return lr
 
+
 def learn_svm(training_input, training_labels):
-    svm = SVC(C=1.0, kernel='poly', degree=7, gamma='auto', coef0=0.0, shrinking=True, probability=False, tol=0.001, cache_size=200, class_weight=None, verbose=False, max_iter=-1, decision_function_shape=None, random_state=None)
+    # TODO: narrow down to necessary params
+    svm = SVC(C=1.0, kernel='poly', degree=7, gamma='auto', coef0=0.0, shrinking=True, probability=False, tol=0.001,
+              cache_size=200, class_weight=None, verbose=False, max_iter=-1, decision_function_shape=None,
+              random_state=None)
     svm.fit(training_input, training_labels)
     return svm
-    
+
+
+def learn_lin_svm(training_input, training_labels):
+    svm = LinearSVC(penalty='l2', loss='squared_hinge', dual=False, tol=0.0001, multi_class='ovr', fit_intercept=True,
+                    class_weight='balanced', max_iter=1000)
+    svm.fit(training_input, training_labels)
+    return svm
+
+
 def calculate_accuracy(model, test_imgs, test_labels):
     return model.score(test_imgs, test_labels)
 
@@ -70,7 +88,7 @@ def classify(model, test_img, emotions):
 
 
 if __name__ == '__main__':
-    training_data = sio.loadmat('C:\\Users\\dhyey_000\\Desktop\\CSC412\\CSC412Project\\labeled_images.mat')
+    training_data = sio.loadmat('labeled_images.mat')
 
     # ith image is training_imgs[:,:,i], and its label is emotions[training_labels[i][0]]
     training_imgs = training_data['tr_images']
@@ -78,20 +96,34 @@ if __name__ == '__main__':
 
     emotions = {1: 'Anger', 2: 'Disgust', 3: 'Fear', 4: 'Happy', 5: 'Sad', 6: 'Surprise', 7: 'Neutral'}
 
-    train_input, train_labels = get_input(training_imgs[:,:,:2500], training_labels[:2500])
-    test_input, test_labels = get_input(training_imgs[:,:,2500:], training_labels[2500:])
+    train_imgs = training_imgs[:,:,:2500]
+    train_labels = training_labels[:2500]
+    test_imgs = training_imgs[:,:,:2500]
+    test_labels = training_labels[:2500]
 
-    # knn = learn_knn(train_input, train_labels, 5)
-    # print calculate_accuracy(knn, test_input, test_labels)
+    train_input, train_targets, train_scaler = get_input(train_imgs, train_labels)
+    test_input, test_targets, _ = get_input(test_imgs, test_labels, train_scaler)
 
-    # nn = learn_nn(train_input, train_labels)
-    # print calculate_accuracy(nn, test_input, test_labels)
+    train_input_pca, train_targets_pca, train_pca = get_input_pca(train_imgs, train_labels)
+    test_input_pca, test_targets_pca, _ = get_input(test_imgs, test_labels, train_pca)
 
-    # log_reg = learn_log_reg(train_input, train_labels)
-    # print calculate_accuracy(log_reg, test_input, test_labels)
+    # knn = learn_knn(train_input, train_targets, 5)
+    # print calculate_accuracy(knn, test_input, test_targets)
 
-    # lin_reg = learn_lin_reg(train_input, train_labels)
-    # print calculate_accuracy(lin_reg, test_input, test_labels)
+    # nn = learn_nn(train_input, train_targets)
+    # print calculate_accuracy(nn, test_input, test_targets)
 
-    svm = learn_svm(train_input, train_labels)
-    print calculate_accuracy(svm, test_input, test_labels)
+    # log_reg = learn_log_reg(train_input, train_targets)
+    # print calculate_accuracy(log_reg, test_input, test_targets)
+
+    log_reg_pca = learn_log_reg(train_input_pca, train_targets_pca)
+    print calculate_accuracy(log_reg_pca, test_input_pca, test_targets_pca)
+
+    # lin_reg = learn_lin_reg(train_input, train_targets)
+    # print calculate_accuracy(lin_reg, test_input, test_targets)
+
+    # lin_svm = learn_lin_svm(train_input, train_targets)
+    # print calculate_accuracy(lin_svm, test_input, test_targets)
+
+    # svm = learn_svm(train_input, train_targets)
+    # print calculate_accuracy(svm, test_input, test_targets)
